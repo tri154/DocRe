3afc8d20d54c8e19ebb2d145c4015074a8447b4e
tri154 <triledacminh@gmail.com>
2025-08-20 17:54:42 +0700

    init

--- src/models/moe_sparse.py
+++ src/models/moe_sparse.py
@@ -32,65 +32,18 @@ class MoeSparse(nn.Module):
 
         self.gate = nn.Bilinear(in1_features, in2_features, num_experts)
         self.noise = nn.Bilinear(in1_features, in2_features, num_experts)
-        # nn.init.zeros_(self.gate.weight)
-        # nn.init.zeros_(self.gate.bias)
-        # nn.init.zeros_(self.noise.weight)
-        # nn.init.zeros_(self.noise.bias)
+        nn.init.zeros_(self.gate.weight)
+        nn.init.zeros_(self.gate.bias)
+        nn.init.zeros_(self.noise.weight)
+        nn.init.zeros_(self.noise.bias)
 
+    def forward(self, h_rep ,t_rep, is_training=True, noise_epsilon=1e-2):
+        # NOTE: be carefull when modify.
+        return self.forward_not_dispatch(h_rep, t_rep, is_training=is_training)
 
-    def reset_stats(self):
-        self.stats = torch.zeros(self.num_experts)
-
-    def set_more_logging(self, value):
-        self.more_logging = value
-
-
-
-    def forward_old(self, h_rep, t_rep):
-        self.clean_forward(h_rep, t_rep)
-        input("STTTTOOPPP")
-        # +===============
-        device = h_rep.device
-
-        # normal noise
-        gate_logits = self.gate(h_rep, t_rep)
-        noise_logits = torch.randn(gate_logits.shape).to(device) * F.softplus(self.noise(h_rep, t_rep))
-        gate_logits = gate_logits + noise_logits
-
-        # uniform noise
-        # noise = torch.rand_like(gate_logits) * self.noise_scale
-        # noise = (torch.rand_like(gate_logits) - 0.5) * 2 * self.noise_scale
-        # gate_out = F.softmax(gate_logits, dim=1)
-        # gate_out = gate_out + noise
-        # gate_out = gate_out / gate_out.sum(dim=-1, keepdim=True)
-
-        # logging
-        if self.more_logging:
-            self.cfg.logging(f"{F.softmax(gate_logits)} ")
-        temp = F.one_hot(torch.argmax(gate_logits.detach(), dim=-1), num_classes=gate_logits.shape[-1]).int()
-        self.stats = self.stats.cpu() + temp.sum(dim=0).cpu()
-        # end logging
-
-        gate_topk, indices = torch.topk(gate_logits, self.topk, dim=-1)
-        gate_out = F.softmax(gate_topk, dim=1) # nsample, topk
-
-        n_sample = h_rep.shape[0]
-        out = list()
-        for sample_id in range(n_sample):
-            expert_ids = indices[sample_id]
-            sample_gate = gate_out[sample_id]
-            sample_out = torch.stack([self.experts[i](h_rep[sample_id], t_rep[sample_id]) for i in expert_ids])
-            out.append(sample_gate @ sample_out)
-
-        out = torch.stack(out)
-        print(gate_out.shape)
-        print(gate_out)
-        input()
-        # todo: gate_out modify ???
-        return out, gate_out
 
     def forward_not_dispatch(self, h_rep ,t_rep, is_training=True, noise_epsilon=1e-2):
-        # NOTE: add load balance loss, if needed.
+        # NOTE: add load balance loss, if need.
         gate_logits = self.gate(h_rep, t_rep)
         if is_training:
             noise_stddev = F.softplus(self.noise(h_rep, t_rep)) + noise_epsilon
@@ -120,10 +73,28 @@ class MoeSparse(nn.Module):
 
         return out, gates
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     def forward_dispatch(self, h_rep ,t_rep, is_training=True, noise_epsilon=1e-2):
         # NOTE: add load balance loss, if needed.
+        # NOTE: currently not in use.
         gate_logits = self.gate(h_rep, t_rep)
-        if is_training and False:
+        if is_training:
             noise_stddev = F.softplus(self.noise(h_rep, t_rep)) + noise_epsilon
             noise_logits = torch.randn(gate_logits.shape).to(gate_logits.device) * noise_stddev
             gate_logits = gate_logits + noise_logits
@@ -191,6 +162,8 @@ class MoeSparse(nn.Module):
 
         return res, gates
 
+    def reset_stats(self):
+        self.stats = torch.zeros(self.num_experts)
 
-    def forward(self, h_rep ,t_rep, is_training=True, noise_epsilon=1e-2):
-        return self.forward_not_dispatch(h_rep, t_rep, is_training=is_training)
+    def set_more_logging(self, value):
+        self.more_logging = value
