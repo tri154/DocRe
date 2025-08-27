a2d6f9e24dc208d419f58f5dbc466a62f9b724f2
tri154 <triledacminh@gmail.com>
2025-08-27 16:29:02 +0700

    more infor

--- src/config.py
+++ src/config.py
@@ -58,7 +58,7 @@ def parse_args():
     parser.add_argument('--importance_weight', type=float, default=1.0)
     parser.add_argument('--use_gate_loss', type=str2bool, default=True)
     parser.add_argument('--gatel_weight', type=float, default=0.1, help="for gate loss weight.")
-    parser.add_argument('--warmup_phase', type=int, default=1, help="number of warmup epoch.")
+    parser.add_argument('--warmup_phase', type=int, default=2, help="number of warmup epoch.")
 
     parser.add_argument('--use_psd', type=str2bool, default=True)
     parser.add_argument('--lower_temp', type=float, default=2.0)
