45fa1fb68eedd7c67c2cec6770151b496a8ad092
tri154 <triledacminh@gmail.com>
2025-08-29 08:26:38 +0700

    modify re

--- src/models/re.py
+++ src/models/re.py
@@ -5,14 +5,23 @@ class RE(nn.Module):
 
     def __init__(self, input_dim, output_dim):
         super(RE, self).__init__()
-        self.bilinear = nn.Bilinear(input_dim, input_dim, input_dim // 2)
-        self.re = nn.Sequential(
-            nn.Linear(input_dim // 2, input_dim // 4),
-            nn.LayerNorm(input_dim // 4),
+        self.w_h = nn.Sequential(
+            nn.Linear(input_dim, input_dim // 2),
+            nn.LayerNorm(input_dim // 2),
             nn.Tanh(),
-            nn.Linear(input_dim // 4, output_dim)
+            nn.Linear(input_dim // 2, input_dim // 4)
         )
+        self.w_t = nn.Sequential(
+            nn.Linear(input_dim, input_dim // 2),
+            nn.LayerNorm(input_dim // 2),
+            nn.Tanh(),
+            nn.Linear(input_dim // 2, input_dim // 4)
+        )
+        self.bilinear = nn.Bilinear(input_dim // 4, input_dim // 4, output_dim)
+
+
     def forward(self, h, t):
-        out = torch.tanh(self.bilinear(h, t))
-        out = self.re(out)
+        h_rep = self.w_h(h)
+        t_rep = self.w_t(t)
+        out = self.bilinear(h_rep, t_rep)
         return out
